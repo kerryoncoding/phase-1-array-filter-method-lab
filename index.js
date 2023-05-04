06afcc9e3538d1d4a636101ea29164ac3b839e2e
Kerry Guarino <kerry.guarino@gmail.com>
2023-05-04 18:51:07 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+
+function findMatching(driverArray, matchName){
+   let newNameArray = []
+   driverArray.filter(function (element) { if (element.toLowerCase() === matchName.toLowerCase()){
+      newNameArray.push(element)
+   }
+   })
+   return newNameArray
+}
+
+function fuzzyMatch(driverArray, firstLetters) {
+   let newNameArray = []
+   driverArray.filter(function(element){if (element.substring(0,firstLetters.length) === firstLetters){
+      newNameArray.push(element)
+   }
+   })
+   return newNameArray
+}
+
+function matchName(driverArray, match) {
+   let newNameArray = []
+   driverArray.filter(function(element){if (element.name === match) {
+      newNameArray.push(element)
+   }
+})
+   return newNameArray
+}
